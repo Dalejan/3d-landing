e22f0813f18844b7b1c8b85ff3cee3abd95ac643
Dalejan <david1804997@gmail.com>
2020-04-17 00:58:24 -0500

    Vista previa, url de backend y detalles

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-export const server = "https://git.heroku.com/frozen-cliffs-38888.git"; //"http://192.168.0.14:8080/";
+export const server = "https://l3d-backend.herokuapp.com/"; //"http://192.168.0.14:8080/";
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
