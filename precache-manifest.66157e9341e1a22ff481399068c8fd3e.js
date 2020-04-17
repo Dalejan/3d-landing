367d22dc1199e54aa2029880ba78dac298d4c16b
Dalejan <david1804997@gmail.com>
2020-04-17 00:46:06 -0500

    Updates

---
+++ precache-manifest.66157e9341e1a22ff481399068c8fd3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19d2e9f699f4127231447df578e31b7",
+    "url": "/3d-landing/index.html"
+  },
+  {
+    "revision": "ea72299e7f71828041c6",
+    "url": "/3d-landing/static/css/main.5a8efe54.chunk.css"
+  },
+  {
+    "revision": "0f7a8680a91568658dd4",
+    "url": "/3d-landing/static/js/2.246eaa9d.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/3d-landing/static/js/2.246eaa9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea72299e7f71828041c6",
+    "url": "/3d-landing/static/js/main.7bb45332.chunk.js"
+  },
+  {
+    "revision": "edeef6c62a68007ce208",
+    "url": "/3d-landing/static/js/runtime-main.0eee4f8d.js"
+  },
+  {
+    "revision": "5d9a9f856769f227cac6cab50cad6f71",
+    "url": "/3d-landing/static/media/logo.5d9a9f85.svg"
+  }
+]);
