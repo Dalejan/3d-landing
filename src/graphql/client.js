208105aaed54e123f310e134849481839b68a8f1
Dalejan <david1804997@gmail.com>
2020-04-17 20:25:35 -0500

    prueba SEO

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-export const server = "https://l3d-backend.herokuapp.com/"; //"http://192.168.0.14:8080/";
+export const server = "http://192.168.0.14:8080/";
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
